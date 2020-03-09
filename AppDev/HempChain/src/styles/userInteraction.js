ea2ab4b14f0728378208837ac86476074a2656d6
Chris Schultz <cschultz5923@gmail.com>
2020-03-09 16:01:31 -0600

    fixing minor formatting issues

--- AppDev/HempChain/src/styles/userInteraction.js
+++ AppDev/HempChain/src/styles/userInteraction.js
@@ -1,12 +1,12 @@
 // the style collection for all user interaction objects
 export const inputBox = {
     width: 300,
-    backgroundColor: 'rgba(255, 255, 255, 0.3)',
+    backgroundColor: 'rgba(255,255,255,0.3)',
     borderRadius: 25,
     paddingHorizontal: 16,
     marginVertical: 10,
     height: 50,
-    justifyContent: 'center'
+    justifyContent: 'center',
 };
 
 export const inputText = {

455d1d045482cc7e73da957a54b84fb386944fc3
Andey Robins <andeyrobins@gmail.com>
2020-03-25 13:20:37 -0600

    App Connects to Localhost

--- AppDev/HempChain/src/utility/webHelper.js
+++ AppDev/HempChain/src/utility/webHelper.js
@@ -2,7 +2,7 @@ const axios = require('react-native-axios');
 
 export async function sendToServer(message) {
     console.log(message);
-    axios.get('https://10.0.2.2:5000/api/books')
+    axios.get('http://10.0.2.2:5000/api/books')
         .then(response => {
             console.log(response.data);
             console.log("done");
